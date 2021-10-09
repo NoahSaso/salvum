9da2e74a60d24d79de6341e110cfe6a7b88ecd8a
Noah Saso <noahsaso@berkeley.edu>
2021-10-08 20:46:18 -0700

    breathe app fully works

--- pages/apps/breathe.tsx
+++ pages/apps/breathe.tsx
@@ -1,24 +1,94 @@
-import { FC } from 'react'
+import cn from 'classnames'
+import { FC, useEffect, useState } from 'react'
 
 import Header from '../../components/apps/header'
 import styles from './breathe.module.scss'
 
-const Breathe: FC = () => (
-  <div className={styles.container}>
-    <Header title="Breathe" />
+enum BreatheState {
+  NotStarted,
+  Inhale,
+  HoldIn,
+  Exhale,
+  HoldOut
+}
+const nextBreatheState: Record<BreatheState, BreatheState> = {
+  [BreatheState.NotStarted]: BreatheState.Inhale,
+  [BreatheState.Inhale]: BreatheState.HoldIn,
+  [BreatheState.HoldIn]: BreatheState.Exhale,
+  [BreatheState.Exhale]: BreatheState.HoldOut,
+  [BreatheState.HoldOut]: BreatheState.Inhale
+}
+const breatheTextMap: Record<BreatheState, string> = {
+  [BreatheState.NotStarted]: 'Get ready...',
+  [BreatheState.Inhale]: 'Inhale through your nose',
+  [BreatheState.HoldIn]: 'Hold',
+  [BreatheState.Exhale]: 'Exhale through your mouth',
+  [BreatheState.HoldOut]: 'Hold'
+}
+const breatheCountMap: Record<BreatheState, number> = {
+  [BreatheState.NotStarted]: 3,
+  [BreatheState.Inhale]: 4,
+  [BreatheState.HoldIn]: 4,
+  [BreatheState.Exhale]: 4,
+  [BreatheState.HoldOut]: 4
+}
 
-    <div className={styles.breatheContainer}>
-      <div className={styles.outer}>
-        <div className={styles.middle}>
-          <div className={styles.inner}>
-            <p>3</p>
+const Breathe: FC = () => {
+  const [breathingIn, setBreathingIn] = useState(false)
+  const [breathingOut, setBreathingOut] = useState(false)
+  const [count, setCount] = useState(breatheCountMap[BreatheState.NotStarted])
+  const [instructions, setInstructions] = useState(breatheTextMap[BreatheState.NotStarted])
+
+  useEffect(() => {
+    let t = 0
+    let state: BreatheState = BreatheState.NotStarted
+    const tick = () => {
+      t++
+
+      const topCount = state === BreatheState.NotStarted ? 3 : 4
+      if (t >= topCount) {
+        t = 0
+        state = nextBreatheState[state]
+        setCount(breatheCountMap[state])
+        setInstructions(breatheTextMap[state])
+        // update animation if necessary
+        switch (state) {
+          case BreatheState.Inhale:
+            setBreathingIn(true)
+            setBreathingOut(false)
+            break
+          case BreatheState.Exhale:
+            setBreathingOut(true)
+            setBreathingIn(false)
+            break
+          default:
+            break
+        }
+      }
+      else setCount(topCount - t)
+    }
+
+    const interval = setInterval(tick, 1000)
+    return () => clearInterval(interval)
+  }, [setBreathingIn, setBreathingOut, setCount])
+
+  return (
+    <div className={styles.container}>
+      <Header title="Breathe" />
+
+      <div className={styles.breatheContainer}>
+        <div className={cn(styles.outer, { [styles.breatheIn]: breathingIn, [styles.breatheOut]: breathingOut })}>
+          <div className={styles.middle}>
+            <div className={styles.inner}>
+              <p>{count}</p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <p className={styles.instructions}></p>
+        <p className={styles.instructions}>{instructions}</p>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Breathe
--- pages/apps/index.tsx
+++ pages/apps/index.tsx
@@ -1,14 +1,10 @@
 import Link from 'next/link'
-import { usePlausible } from 'next-plausible'
 import { FC } from 'react'
 import { FiChevronRight } from 'react-icons/fi'
 
-import { PlausibleEvents } from '../../types'
 import styles from './index.module.scss'
 
 const Apps: FC = () => {
-  const plausible = usePlausible<PlausibleEvents>()
-
   return (
     <div className={styles.container}>
       <h1>Apps</h1>
