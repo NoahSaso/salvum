2b445a3c8b06cd137134ece95e36f7229adbca68
Noah Saso <noahsaso@gmail.com>
2022-01-24 15:18:31 -0800

    added title to emergency page

--- pages/emergency.tsx
+++ pages/emergency.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import Link from "next/link"
 import React, { FC } from "react"
 
@@ -23,28 +24,38 @@ const pages = [
 ]
 
 const EmergencyPage: FC = () => (
-  <div className={styles.container}>
-    <h1>Select the type of emergency.</h1>
+  <>
+    <Head>
+      <title>Salvum | Emergency</title>
+      <meta
+        name="description"
+        content="Guides to help make it through a difficult experience."
+      />
+    </Head>
 
-    <div className={styles.pages}>
-      {pages.map(({ title, href }) => (
-        <Link key={title} href={href}>
-          <a>{title}</a>
-        </Link>
-      ))}
+    <div className={styles.container}>
+      <h1>Select the type of emergency.</h1>
+
+      <div className={styles.pages}>
+        {pages.map(({ title, href }) => (
+          <Link key={title} href={href}>
+            <a>{title}</a>
+          </Link>
+        ))}
+      </div>
+
+      <p>
+        Disclaimer:
+        <br />
+        The information contained within these pages is intended for educational
+        purposes only. It is your responsibility to do your own research and
+        ensure you know what steps to take in the event of a dangerous
+        situation. It&lsquo;s better to be safe than sorry, so when in doubt,
+        contact emergency health services if anyone is at risk of hurting
+        themself or others.
+      </p>
     </div>
-
-    <p>
-      Disclaimer:
-      <br />
-      The information contained within these pages is intended for educational
-      purposes only. It is your responsibility to do your own research and
-      ensure you know what steps to take in the event of a dangerous situation.
-      It&lsquo;s better to be safe than sorry, so when in doubt, contact
-      emergency health services if anyone is at risk of hurting themself or
-      others.
-    </p>
-  </div>
+  </>
 )
 
 export default EmergencyPage
