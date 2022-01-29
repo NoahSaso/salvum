57a8eb14f47588057192c86ca22ab7f9bf3b9aaf
Noah Saso <noahsaso@gmail.com>
2022-01-29 12:11:17 -0800

    formatted js config files

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "presets": ["next/babel"],
-  "plugins": ["@emotion"],
+  presets: ["next/babel"],
+  plugins: ["@emotion"],
 }
--- next.config.js
+++ next.config.js
@@ -1,31 +1,30 @@
 /** @type {import('next').NextConfig} */
 
-const { withPlausibleProxy } = require('next-plausible')
-const withPWA = require('next-pwa')
-const path = require('path')
+const { withPlausibleProxy } = require("next-plausible")
+const withPWA = require("next-pwa")
+const path = require("path")
 
-module.exports =
-  withPWA({
-    pwa: {
-      dest: 'public'
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+  },
+  images: {
+    domains: ["i.creativecommons.org"],
+  },
+  ...withPlausibleProxy({
+    customDomain: "https://plausible.noahsaso.com",
+  })({
+    reactStrictMode: true,
+    sassOptions: {
+      includePaths: [path.join(__dirname, "assets")],
     },
-    images: {
-      domains: ['i.creativecommons.org']
+    async rewrites() {
+      return [
+        {
+          source: "/s/:substance",
+          destination: "/",
+        },
+      ]
     },
-    ...withPlausibleProxy({
-      customDomain: "https://plausible.noahsaso.com"
-    })({
-      reactStrictMode: true,
-      sassOptions: {
-        includePaths: [path.join(__dirname, 'assets')]
-      },
-      async rewrites() {
-        return [
-          {
-            source: '/s/:substance',
-            destination: '/'
-          }
-        ]
-      }
-    })
-  })
+  }),
+})
